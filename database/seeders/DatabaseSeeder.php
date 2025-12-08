1be602a1b838d24c49cea14e53623079d0530e10
Demi Oyewusi <tayodemi@gmail.com>
2025-12-08 15:35:58 +0100

    fixes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(5)->create();
+        //User::factory(5)->create();
+
+        $this->call(UserSeeder::class);
 
         foreach (range(1, 5) as $userId) {
             StoreFront::factory()->create([
